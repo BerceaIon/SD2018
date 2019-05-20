3e6be681448205ecdbd84bfb2416df2ffe62d5b3
Bercea <berceai63@gmail.com>
2019-05-20 17:14:20 +0300

    assig2

---
+++ assig2/src/main/java/com/bercea/assigment2/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bercea.assigment2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bercea.assigment2.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
